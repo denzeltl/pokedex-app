bf1c8c3ef24707656e025e6326211ffc89a0f4a1
Denzel <denzeltlee@gmail.com>
2020-07-25 18:00:47 +0800

    Updates

---
+++ precache-manifest.2b078e6c825be21e1f1676c56e142d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be33d9ee782741822615192c83c5bdf",
+    "url": "/pokedex-app/index.html"
+  },
+  {
+    "revision": "08ebe077f43e5dab2621",
+    "url": "/pokedex-app/static/css/main.8fd4f0bb.chunk.css"
+  },
+  {
+    "revision": "8d35fc80b77bc9a74022",
+    "url": "/pokedex-app/static/js/2.eb0d2fe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-app/static/js/2.eb0d2fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ebe077f43e5dab2621",
+    "url": "/pokedex-app/static/js/main.28156d7f.chunk.js"
+  },
+  {
+    "revision": "8f5e4f66b90e80d99a65",
+    "url": "/pokedex-app/static/js/runtime-main.3edb0aa6.js"
+  }
+]);
